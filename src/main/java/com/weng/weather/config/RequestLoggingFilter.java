d6f6c145eb6edf690a36f112651a498a522a891d
Robert Lee <leekw.robert@gmail.com>
2025-01-13 16:08:56 +0800

    feat: Implement logging for incoming request and response

--- src/main/java/com/weng/weather/config/RequestLoggingFilter.java
+++ src/main/java/com/weng/weather/config/RequestLoggingFilter.java
@@ -5,13 +5,17 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
 import java.util.UUID;
 
-@WebFilter("/*") // Apply the filter to all incoming requests
+@WebFilter("/*")
+@Slf4j
 public class RequestLoggingFilter extends OncePerRequestFilter {
 
     public static final String REQUEST_UUID_KEY = "requestUUID";
@@ -25,12 +29,37 @@ public class RequestLoggingFilter extends OncePerRequestFilter {
         // Put the UUID in MDC
         MDC.put(REQUEST_UUID_KEY, requestUUID);
 
+        // Wrap request and response for logging purposes
+        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper wrappedResponse = new ContentCachingResponseWrapper(response);
+
+        long startTime = System.currentTimeMillis();
+
         try {
-            // Continue the filter chain
+            logServerRequest(wrappedRequest);
+
             chain.doFilter(request, response);
+
+            logServerResponse(wrappedResponse, startTime);
         } finally {
+            wrappedResponse.copyBodyToResponse();
             // Clear the MDC after the request has been processed
-            MDC.remove("requestUUID");
+            MDC.remove(REQUEST_UUID_KEY);
         }
+    }
+
+    private void logServerRequest(ContentCachingRequestWrapper request) {
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        String queryString = request.getQueryString();
+
+        log.info("SERVER-REQ: Method: {}, URI: {}, Query: {}",
+                method, uri, queryString != null ? queryString : "[Empty Query]");
+    }
+
+    private void logServerResponse(ContentCachingResponseWrapper response, long startTime) {
+        long duration = System.currentTimeMillis() - startTime;
+        log.info("SERVER-RES: Duration: {} ms, Status: {}",
+                duration, response.getStatus());
     }
 }
